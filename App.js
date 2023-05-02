4c258d676826b28e0054b2fec2f051bcc7a2e73c
Pierex99 <piero_gd99@outlook.com>
2023-05-01 23:51:56 -0500

    lista funcionando

--- App.js
+++ App.js
@@ -1,28 +1,97 @@
-import { Button, StyleSheet, TextInput, Text, View } from 'react-native';
+import { Button, StyleSheet, TextInput, Text, View, FlatList } from 'react-native';
+import { useState } from 'react';
+import Modal from './src/components/Modal';
 
 export default function App() {
+  const [textItem, setTextItem] = useState("")
+  const [list, setList] = useState([])
+  const [itemSelected, setItemSelected] = useState({});
+  const [modalVisible, setModalVisible] = useState(false);
+
+
+  const onHandleChangeText = (text) => {
+    setTextItem(text)
+    console.log(text)
+  }
+
+  const addItem = () => {
+    console.log("aqui agregamos item", textItem)
+    setList(prevState => [...prevState, { name: textItem, id: Math.random().toString() },
+    ]);
+    setTextItem("");
+  }
+
+  const onHandleModal = item => {
+    console.log("en esta funcion seteo el item y abro modal")
+    setItemSelected(item)
+    setModalVisible(true)
+  }
+
+  const onHandleDelete = id => {
+    console.log("eliminar este item", item)
+    setList(prevState => 
+      prevState.filter(element => element !== item.name))
+    setModalVisible(false)
+  }
+
+  const renderItem = ({ item }) => (
+    <View style={styles.renderItemStyle}>
+      <Text>{item.name}</Text>
+      <Button
+        title="X"
+        onPress={() => onHandleModal(item)}
+        color={"red"}
+      />
+    </View>
+  )
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput
-          placeholder='elemento de la lista'
-          style={styles.input}
-        />
-        <Button 
-          title="Presiona aqui"
-          onPress={() => console.log("presiona aqui")}
+        <Text style={styles.titleContainer}>Shopping List</Text>
+        <View style={styles.addItemContainer}>
+          <TextInput
+            placeholder='elemento de la lista'
+            style={styles.input}
+            onChangeText={onHandleChangeText}
+            value={textItem}
+          />
+          <Button title="Presiona aqui" onPress={addItem} />
+        </View>
+      </View>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={list}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
         />
       </View>
-      <View></View>
+      <Modal
+        isVisible={modalVisible}
+        actionDeleteItem={() => onHandleDelete(itemSelected)}
+        itemSelected={itemSelected}
+      />  
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 30,
+    flex: 1,
+    backgroundColor: "#E7EAF2"
   },
   inputContainer: {
+    height: 200,
+    paddingHorizontal: 30,
+    paddingTop: 80,
+  },
+  titleContainer: {
+    marginBottom: 30,
+    fontSize: 40,
+    fontWeight: "500",
+    color: "#1E283C"
+  },
+  addItemContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
@@ -32,28 +101,25 @@ const styles = StyleSheet.create({
     borderBottomWidth: 2,
     width: 200,
   },
-});
-
-/*import { StyleSheet, Text, View } from "react-native";
-
-import React from "react";
-
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <View style={{ flex: 2, backgroundColor: "green" }}></View>
-      <View style={{ flex: 1, backgroundColor: "blue" }}></View>
-      <View style={{ flex: 1, backgroundColor: "yellow" }}></View>
-    </View>
-  );
-};
-
-export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "red",
+  listContainer: {
+    flex: 2, 
+    marginHorizontal: 30,
+    marginTop: 20,
+    padding: 3,
   },
+  renderItemStyle: {
+    height: 60,
+    flexDirection: 'row',
+    marginBottom: 25,
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 10,
+    justifyContent: "space-around",
+    alignItems: "center",
+    shadowColor: "black",
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 0, height: 1},
+    shadowRadius: 2,
+    elevation: 3,
+  }
 });
-*/
